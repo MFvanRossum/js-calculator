1b8dae8ae303c413af158ff1a733c1a8841f7888
MFvanRossum <matissev@hotmail.com>
2020-01-30 09:59:09 -0500

    formats code

--- main.js
+++ main.js
@@ -6,112 +6,112 @@
 // }
 
 let button0 = document.querySelector('.zero')
-button0.addEventListener('click', function outputCalc (){
+button0.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode(0)
     output.append(newOutput)
 })
 
 let button1 = document.querySelector('.one')
-button1.addEventListener('click', function outputCalc (){
+button1.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode(1)
     output.append(newOutput)
 })
 
 let button2 = document.querySelector('.two')
-button2.addEventListener('click', function outputCalc (){
+button2.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode(2)
     output.append(newOutput)
 })
 
 let button3 = document.querySelector('.three')
-button3.addEventListener('click', function outputCalc (){
+button3.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode(3)
     output.append(newOutput)
 })
 
 let button4 = document.querySelector('.four')
-button4.addEventListener('click', function outputCalc (){
+button4.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode(4)
     output.append(newOutput)
 })
 
 let button5 = document.querySelector('.five')
-button5.addEventListener('click', function outputCalc (){
+button5.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode(5)
     output.append(newOutput)
 })
 
 let button6 = document.querySelector('.six')
-button6.addEventListener('click', function outputCalc (){
+button6.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode(6)
     output.append(newOutput)
-}) 
+})
 
 let button7 = document.querySelector('.seven')
-button7.addEventListener('click', function outputCalc (){
+button7.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode(7)
     output.append(newOutput)
 })
 
 let button8 = document.querySelector('.eight')
-button8.addEventListener('click', function outputCalc (){
+button8.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode(8)
     output.append(newOutput)
 })
 
 let button9 = document.querySelector('.nine')
-button9.addEventListener('click', function outputCalc (){
+button9.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode(9)
     output.append(newOutput)
 })
 
 let buttonDecimal = document.querySelector('.decimal')
-buttonDecimal.addEventListener('click', function outputCalc (){
+buttonDecimal.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode('.')
     output.append(newOutput)
 })
 
 let buttonDiv = document.querySelector('.divide')
-buttonDiv.addEventListener('click', function outputCalc (){
+buttonDiv.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode('/')
     output.append(newOutput)
 })
 
 let buttonMult = document.querySelector('.multiply')
-buttonMult.addEventListener('click', function outputCalc (){
+buttonMult.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode('*')
     output.append(newOutput)
 })
 
 let buttonSub = document.querySelector('.subtract')
-buttonSub.addEventListener('click', function outputCalc (){
+buttonSub.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode('-')
     output.append(newOutput)
 })
 
 let buttonAdd = document.querySelector('.add')
-buttonAdd.addEventListener('click', function outputCalc (){
+buttonAdd.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = document.createTextNode('+')
     output.append(newOutput)
 })
 
 let buttonEq = document.querySelector('.equals')
-buttonEq.addEventListener('click', function outputCalc (){
+buttonEq.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let outputEq = output.innerText
     let ans = eval(outputEq)
@@ -119,7 +119,7 @@ buttonEq.addEventListener('click', function outputCalc (){
 })
 
 let buttonC = document.querySelector('.clear')
-buttonC.addEventListener('click', function outputCalc (){
+buttonC.addEventListener('click', function outputCalc() {
     let output = document.querySelector('.output')
     let newOutput = ""
     output.innerHTML = newOutput
